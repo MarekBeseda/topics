b85760168ba33cc34dfdfa70c32fc89566353a0c
Jedrzej Blaszyk <jedrzej.blaszyk16@imperial.ac.uk>
2017-03-03 14:30:24 +0000

    Picture crops

--- Gruntfile.js
+++ Gruntfile.js
@@ -89,10 +89,10 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 9001,
+        port: 9000,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: 'localhost',
-        livereload: 35728
+        livereload: 35729
       },
       livereload: {
         options: {
@@ -116,7 +116,7 @@ module.exports = function (grunt) {
       },
       test: {
         options: {
-          port: 9001,
+          port: 9000,
           middleware: function (connect) {
             return [
               connect.static('.tmp'),
