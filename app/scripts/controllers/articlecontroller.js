50778c656b2eb1d3b65f1d0aa09b975131404a8d
Marek Beseda <marek.beseda16@gmail.com>
2017-03-12 16:22:37 +0000

    smooth scroll, double tile

--- app/scripts/controllers/articlecontroller.js
+++ app/scripts/controllers/articlecontroller.js
@@ -8,19 +8,8 @@
  * Controller of the icstopicsApp
  */
 angular.module('icstopicsApp')
-  .controller('ArticleCtrl', ['$anchorScroll', '$location', '$scope',
-  function($anchorScroll, $location, $scope) {
-    $scope.gotoAnchor = function(x) {
-      var newHash = 'anchor' + x;
-      if ($location.hash() !== newHash) {
-        // set the $location.hash to `newHash` and
-        // $anchorScroll will automatically scroll to it
-        $location.hash('anchor' + x);
-      } else {
-        // call $anchorScroll() explicitly,
-        // since $location.hash hasn't changed
-        $anchorScroll();
-      }
-    };
+  .controller('ArticleCtrl',
+  function() {
+    
   }
-]);
+);
