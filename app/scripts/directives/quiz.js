07fee3ec0a9b286534469c74955747b5d093eee9
Marek Beseda <marek.beseda16@gmail.com>
2017-03-15 11:54:40 +0000

    Quiz almost done

--- app/scripts/directives/quiz.js
+++ app/scripts/directives/quiz.js
@@ -16,13 +16,26 @@ angular.module('icstopicsApp')
       },
       link: function postLink(scope) {
         scope.page = 0;
+        scope.correct = 0;
         scope.answered = function(index){
           if(scope.definition.questions[scope.page - 1].correct === index){
             scope.lastCorrect = true;
+            scope.correct++;
           } else {
             scope.lastCorrect = false;
           }
           scope.page++;
+        };
+        scope.quizStyle = function(){
+          if(scope.page === 0){
+            return {height: 200};
+          } else if (scope.page === scope.definition.questions.length + 1){
+            return {height: 200};
+          } else if(scope.page === 1){
+            return {height: (100 + scope.definition.questions[scope.page - 1].options.length * 50)};
+          } else {
+            return {height: (150 + scope.definition.questions[scope.page - 1].options.length * 50)};
+          }
         };
       }
     };
